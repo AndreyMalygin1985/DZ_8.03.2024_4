2ba663216e57adefb8e1550b4f26e5a90ce4ff6d
Андрей <dusha22075@gmail.com>
2024-03-08 11:46:20 +0300

    Добавьте файлы проекта.

---
+++ DZ_8.03.2024_4/Program.cs
@@ -0,0 +1,58 @@
+﻿// Создать абстрактный базовый класс Worker (работника) с методом Print(). Создайте четыре
+// производных класса: President, Security, Manager, Engineer. Переопределите метод
+// Print() для вывода информации, соответствующей каждому типу работника.
+
+using static System.Console;
+
+abstract class Worker
+{
+    public abstract void Print();
+}
+
+class President : Worker
+{
+    public override void Print()
+    {
+        WriteLine("President: Высший руководитель предприятия");
+    }
+}
+
+class Security : Worker
+{
+    public override void Print()
+    {
+        WriteLine("Security: Охранник, обеспечивающий безопасность предприятия");
+    }
+}
+
+class Manager : Worker
+{
+    public override void Print()
+    {
+        WriteLine("Manager: Менеджер, управляющий отделом или проектом");
+    }
+}
+
+class Engineer : Worker
+{
+    public override void Print()
+    {
+        WriteLine("Engineer: Инженер, специалист по теническим вопросам");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Worker president = new President();
+        Worker security = new Security();
+        Worker manager = new Manager();
+        Worker engineer = new Engineer();
+
+        president.Print();
+        security.Print();
+        manager.Print();
+        engineer.Print();
+    }
+}
